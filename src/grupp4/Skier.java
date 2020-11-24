8439a148784fa79eaf1fc7c477ef76d964760f5c
Fredrik Filén <fredrikfihlen@gmail.com>
2020-11-24 11:50:43 +0100

    helped abebe with the commit
    added base constructor and properties to the skier class

--- src/grupp4/Skier.java
+++ src/grupp4/Skier.java
@@ -1,6 +1,21 @@
 package grupp4;
 
-
 public class Skier {
+	private String name = "";
+	static int jerseyNumber = 0;
+	private int position = 0;
+
+	public Skier(String name, int position) {
+		this.name = name;
+		this.position = position;
+	}
+
+	public void move() {
+		System.out.println("The competitor is racing.");
+	}
+
+	public void stop() {
+		System.out.println("The competitor finished the race.");
+	}
 
 }
