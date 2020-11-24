b7c397363ed1fd4e707e786c191e63e849f9d8bd
Lolloa <73934033+Lolloa@users.noreply.github.com>
2020-11-24 11:00:11 +0100

    La till meny för tävlingar

--- src/grupp4/MainMenu.java
+++ src/grupp4/MainMenu.java
@@ -7,32 +7,54 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class MainMenu implements ActionListener {
 	JFrame frame = new JFrame();
 	JLabel label1 = new JLabel("Välkommen till vårt skidprogram");
-	JButton newTimerButton = new JButton("Starta timer");
+	JButton individualButton = new JButton("Individuell start");
+	JButton masstartButton = new JButton("Masstart");
+	JButton jaktstartButton = new JButton("Jaktstart");
+	
 
 	MainMenu() {
 		label1.setBounds(400, 50, 500, 100);
 
-		newTimerButton.setBounds(300, 200, 500, 50);
-		newTimerButton.setFont(new Font("Garamond", Font.PLAIN, 20));
-		newTimerButton.setFocusable(false);
-		newTimerButton.addActionListener(this);
+		individualButton.setBounds(50, 200, 250, 50);
+		individualButton.setFont(new Font("Garamond", Font.PLAIN, 20));
+		individualButton.setFocusable(false);
+		individualButton.addActionListener(this);
+		
+		masstartButton.setBounds(350, 200, 250, 50);
+		masstartButton.setFont(new Font("Garamond", Font.PLAIN, 20));
+		masstartButton.setFocusable(false);
+		masstartButton.addActionListener(this);
+		
+		jaktstartButton.setBounds(650, 200, 250, 50);
+		jaktstartButton.setFont(new Font("Garamond", Font.PLAIN, 20));
+		jaktstartButton.setFocusable(false);
+		jaktstartButton.addActionListener(this);
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1000, 1000);
+		frame.setSize(1000, 500);
 		frame.setLayout(null);
 		frame.setVisible(true);
 
 		frame.add(label1);
-		frame.add(newTimerButton);
+		frame.add(individualButton);
+		frame.add(masstartButton);
+		frame.add(jaktstartButton);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == newTimerButton) {
+		if (e.getSource() == individualButton) {
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.start();
+		} else if (e.getSource() == masstartButton) {
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.start();
+		} else if (e.getSource() == jaktstartButton) {
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.start();
 		}
