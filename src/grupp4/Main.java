4962ce6dbd0d4e338a32469491c96cb303fb8097
Fredrik Filén <fredrikfihlen@gmail.com>
2020-12-02 12:10:54 +0100

    added fxml and controller class

--- src/grupp4/Main.java
+++ src/grupp4/Main.java
@@ -1,16 +1,17 @@
 package grupp4;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 400, 400);
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -22,7 +23,8 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 
-		MainMenu mainmenu = new MainMenu();
+		// runs the old swing version of MaunMenu
+		// MainMenu mainmenu = new MainMenu();
 	}
 
 }
---
+++ src/grupp4/SampleController.java
@@ -0,0 +1,11 @@
+package grupp4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SampleController {
+
+	@FXML
+	private Label testLabel;
+
+}
