7569ed940715bf63cbb581822103415205098efe
FredrikFilen <69147419+FredrikFilen@users.noreply.github.com>
2020-12-29 13:24:36 +0100

    variable refactoring and general code cleanup

--- src/grupp4/Main.java
+++ src/grupp4/Main.java
@@ -3,6 +3,7 @@ package grupp4;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -14,6 +15,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			primaryStage.setTitle("Superski x3000 FIS");
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("Assets/skier.png")));
 			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("main.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
